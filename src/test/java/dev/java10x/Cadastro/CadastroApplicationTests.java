a931c02aafa50319b8c17b463923761546db5079
Enthone <enthoneferreira9@gmail.com>
2025-03-24 15:23:09 -0300

    first commit

---
+++ src/test/java/dev/java10x/Cadastro/CadastroApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.java10x.Cadastro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
